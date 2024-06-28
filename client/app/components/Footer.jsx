5c45a3d8eddae41ae1e3cb2d8cf8f99148a33463
JackPangalia <jackpangalia1@gmail.com>
2024-06-27 19:35:31 -0700

    removed social links from the footer

--- client/app/components/Footer.jsx
+++ client/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={`flex sm:flex-row flex-col sm:mt-44 mt-20 sm:gap-24 gap-10 sm:px-[3rem] px-[2rem] max-w-[1500px] mx-auto pb-[6rem]`}>
       <div>
-        <div className="flex gap-2 mb-3">
+        {/* <div className="flex gap-2 mb-3">
           <svg
             viewBox="0 0 1024 1024"
             fill="currentColor"
@@ -23,7 +23,7 @@ const Footer = () => {
           >
             <path d="M941.3 296.1a112.3 112.3 0 00-79.2-79.3C792.2 198 512 198 512 198s-280.2 0-350.1 18.7A112.12 112.12 0 0082.7 296C64 366 64 512 64 512s0 146 18.7 215.9c10.3 38.6 40.7 69 79.2 79.3C231.8 826 512 826 512 826s280.2 0 350.1-18.8c38.6-10.3 68.9-40.7 79.2-79.3C960 658 960 512 960 512s0-146-18.7-215.9zM423 646V378l232 133-232 135z" />
           </svg>
-        </div>
+        </div> */}
         <div className="flex flex-col">
           {/* <span className="">© 2024 Centonis technologies Inc</span> */}
           <span>
